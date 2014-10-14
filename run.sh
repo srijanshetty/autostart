bb45bd5f337ddfc1d569f497a139858581f3f4e8
Srijan R Shetty <srijan.shetty@gmail.com>
2014-10-14 11:08:04 +0530

    run.sh will run the command when only one argument is give

--- run.sh
+++ run.sh
@@ -3,7 +3,13 @@
 if pgrep $1 &> /dev/null; then
     return 2
 else
-    if $2 &> /dev/null &; then
+    if [ -n $2 ]; then
+        APP=$1
+    else
+        APP=$2
+    fi
+
+    if $APP &> /dev/null &; then
         return 0
     else
         return 1
